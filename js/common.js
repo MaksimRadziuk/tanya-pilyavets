6035ed76e3955310f60ee3316d23ffcbceb3aef6
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-03-03 02:40:26 +0300

    03/03 2.40

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   
+    $('header .slick_wrap').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        loop:true,
+        autoplay:true,
+        autoplaySpeed:5000,
+        speed:2000,
+        fade:true
+    });
+
+});
