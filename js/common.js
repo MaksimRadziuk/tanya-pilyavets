12462a8aedca13ea51858a81c71651b6aa1bda74
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-03-04 02:04:04 +0300

    05/03 2.00

--- js/common.js
+++ js/common.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    $('.top_bar li').click(function(){
+        $('.top_bar li').removeClass('active');
+        $(this).addClass('active');
+    });
+
    
     $('header .slick_wrap').slick({
         slidesToShow: 1,
@@ -9,7 +15,73 @@ $(document).ready(function() {
         autoplay:true,
         autoplaySpeed:5000,
         speed:2000,
-        fade:true
+        fade:true,
+        pauseOnFocus:false,
+        pauseOnHover:false
     });
+
+    $('.slide_btn').click(function(){
+        $(this).siblings('.hid_content').slideToggle();
+        $(this).toggleClass('rotate');
+    });
+
+
+    $('.masonry_wrap').masonry({
+      itemSelector: '.tab_content_block',
+      columnWidth: '.tab_content_block'
+    });
+
+    $('#all_tab').click(function(){
+        $(this).siblings('.tab').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab_content').fadeOut().removeClass('active');
+        $('#all_content').fadeIn().addClass("active");
+    });
+    
+    $('#wedding_tab').click(function(){
+        $(this).siblings('.tab').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab_content').fadeOut().removeClass('active');
+        $('#wedding_content').fadeIn().addClass("active");
+    });
+    
+    $('#family_tab').click(function(){
+        $(this).siblings('.tab').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab_content').fadeOut().removeClass('active');
+        $('#family_content').fadeIn().addClass("active");
+    });
+    
+    $('#love_tab').click(function(){
+        $(this).siblings('.tab').removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab_content').fadeOut().removeClass('active');
+        $('#love_content').fadeIn().addClass("active");
+    });
+    
+
+
+    $(document).ready(function(){
+        $("body").on("click",".top_bar a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+    });
+
+    $('.mobile_btn').click(function(){
+        $(this).toggleClass('active');
+        $(this).siblings('.mobile_nav').toggleClass('active');
+    });
+    $('.mobile_nav ul li a').click(function(){
+        $('.mobile_btn').toggleClass('active');
+        $('.mobile_nav').toggleClass('active');
+    });
+    
 
 });
